aab0e4a6f39618813c224f328159f86fe7eba3d6
Anto Vukoje <vukoje.anto1@gmail.com>
2021-08-08 16:48:33 +0200

    Main Menu Update 3
    Fog, sounds and fonts

--- fearofthedark/Assets/Scripts/MenuStart.cs
+++ fearofthedark/Assets/Scripts/MenuStart.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class MenuStart : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     public Canvas startC;
     public Canvas mainC;
 
-    // Use this for initialization
+    public AudioSource playSound;
+
+    int phase = 0;
     void Start()
     {
         startC.GetComponent<Canvas>().enabled = true;
@@ -19,10 +21,14 @@ public class MenuStart : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButton("Submit"))
+        if (Input.GetButton("Submit") && phase==0)
         {
             startC.GetComponent<Canvas>().enabled = false;
             mainC.GetComponent<Canvas>().enabled = true;
+
+            playSound.Play(0);
+
+            phase++;
         }
     }
 }
