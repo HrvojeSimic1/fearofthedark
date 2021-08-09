7c48d07e1085f052280f2bf96df844f825ae24c8
Riopu6 <vale.riopu6@gmail.com>
2021-08-09 17:39:21 +0200

    Ai i damage

---
+++ fearofthedark/Assets/Code/AIComponents.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class AIComponents : MonoBehaviour {
+	
+	public float speed;
+	public Transform target;
+	[HideInInspector] public new Rigidbody rigidbody;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public virtual void Start()
+	{
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+}
