435edeee4ca6f0a2dbe201485f2a7cd22cf5ff2b
Anto Vukoje <vukoje.anto1@gmail.com>
2021-08-28 20:58:53 +0200

    Audio and Textures

--- fearofthedark/Assets/Scripts/MenuStart.cs
+++ fearofthedark/Assets/Scripts/MenuStart.cs
@@ -10,12 +10,15 @@ public class MenuStart : MonoBehaviour
     public Canvas mainC;
 
     public AudioSource playSound;
+    public AudioSource musicPlayer;
 
     int phase = 0;
     void Start()
     {
         startC.GetComponent<Canvas>().enabled = true;
         mainC.GetComponent<Canvas>().enabled = false;
+
+        musicPlayer.Play(0);
     }
 
 
