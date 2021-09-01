237f0095e15a2707e8b5853619f398f5de579fa3
Anto Vukoje <vukoje.anto1@gmail.com>
2021-09-01 16:44:45 +0200

    Polishing

--- fearofthedark/Assets/Scripts/NextLevelTrigger.cs
+++ fearofthedark/Assets/Scripts/NextLevelTrigger.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewLevelTrigger : MonoBehaviour
+public class NextLevelTrigger: MonoBehaviour
 {
 	public Vector3 collision = Vector3.zero;
-	public LayerMask layer;
+	public LayerMask levelTriggerLayer;
 
 	public int range = 100;
-
 	public int levelNumber;
+
 
 	void Update()
 	{
 		if (Input.GetKey("e"))
 		{
 			var ray = new Ray(transform.position, transform.forward);
-			if (Physics.Raycast(ray, out RaycastHit hit, range, layer))
+			if (Physics.Raycast(ray, out RaycastHit hit, range, levelTriggerLayer))
 			{
 				NextScene();
 			}
+
+			
 		}
 	}
 
